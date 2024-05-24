de760f068f6205342cdc03cf4482d6736f4ae92c
Patrik Pop <ppatrik771@gmail.com>
2024-05-24 22:28:15 +0100

    fixed tests

--- test/pgmq.NET.ComponentTests/BaseTest.cs
+++ test/pgmq.NET.ComponentTests/BaseTest.cs
@@ -22,13 +22,10 @@ public class BaseTest
     public async Task Setup()
     {
         _client = new DockerClientConfiguration().CreateClient();
-        await _client.Images.PushImageAsync("quay.io/tembo/pgmq-pg:latest", new ImagePushParameters(), new AuthConfig(), new Progress<JSONMessage>());
         var response = await _client.Containers.CreateContainerAsync(new CreateContainerParameters {
             Image = "quay.io/tembo/pgmq-pg",
             Name = "postgres",
-            Env =  ["POSTGRES_PASSWORD=postgres", "listen_addresses = '*'", 
-                "POSTGRES_HOST_AUTH_METHOD=scram-sha-256",
-                "POSTGRES_INITDB_ARGS=--auth-host=scram-sha-256"],
+            Env =  ["POSTGRES_PASSWORD=postgres", "listen_addresses = '*'"],
             HostConfig = new HostConfig {
                 //Binds = [@"D:/Projects/pgmq.NET/test/pg_hba.conf:/var/lib/postgresql/data/", @"D:/Projects/pgmq.NET/test/postgresql.conf:/var/lib/postgresql/data/"],
                 
@@ -42,15 +39,7 @@ public class BaseTest
             } 
         });
         await _client.Containers.StartContainerAsync(response.ID, new ContainerStartParameters());
-        var execCreateParameters = new ContainerExecCreateParameters
-        {
-            AttachStdout = true,
-            AttachStderr = true,
-            Cmd = new List<string> { "/bin/sh", "-c", $"echo '{ await File.ReadAllTextAsync(@"D:\Projects\pgmq.NET\test\pg_hba.conf")}' > /var/lib/postgresql/data/" }
-        };
 
-        var execCreateResponse = await _client.Exec.ExecCreateContainerAsync(response.ID, execCreateParameters);
-        await _client.Exec.StartContainerExecAsync(execCreateResponse.ID);
         QueueWriter = QueueBuilder.CreateQueueWriter(options => {
             options.ConnectionString = ConnectionString;
             options.QueueName = QueueName;
